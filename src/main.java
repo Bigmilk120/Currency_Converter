a68e9d7cf4b0d40d171247e2d41b19a11b1d3b98
Bigmilk120 <sprawdzany96@gmail.com>
2019-01-04 16:31:05 +0100

    Currency_Converter
    Made some code in background, no viusal

---
+++ src/main.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+
+public class main {
+	public static final String ACCESS_KEY = "5794ecf11594dd5c16b5c4b84eb38336";
+    public static final String BASE_URL = "http://apilayer.net/api/";
+    public static final String ENDPOINT = "live";
+
+    static CloseableHttpClient httpClient = HttpClients.createDefault();
+    
+    public static void sendLiveRequest(){
+
+        HttpGet get = new HttpGet(BASE_URL + ENDPOINT + "?access_key=" + ACCESS_KEY);
+
+        try {
+            CloseableHttpResponse response =  httpClient.execute(get);
+            HttpEntity entity = response.getEntity();
+
+            JSONObject exchangeRates = new JSONObject(EntityUtils.toString(entity));
+
+            System.out.println("Live Currency Exchange Rates");
+
+            Date timeStampDate = new Date((long)(exchangeRates.getLong("timestamp")*1000)); 
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss a");
+            String formattedDate = dateFormat.format(timeStampDate);
+            System.out.println("1 " + exchangeRates.getString("source") + " in GBP : " + exchangeRates.getJSONObject("quotes").getDouble("USDGBP") + " (Date: " + formattedDate + ")");   
+            System.out.println("\n");
+            response.close();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+    public static void main() throws IOException{
+        sendLiveRequest();
+        httpClient.close();
+        new BufferedReader(new InputStreamReader(System.in)).readLine();
+    }
+}
