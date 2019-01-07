9af9fe69bb03da1370b6338b59f31acbed4c8852
Bigmilk120 <sprawdzany96@gmail.com>
2019-01-07 13:21:32 +0100

    Currency Converter
    Got some GUI problems, about to solve them.

--- src/default_package/userTesting.java
+++ src/default_package/userTesting.java
@@ -10,6 +10,7 @@ public class userTesting extends JFrame{
 	 private static Scanner reader;
 	 private static JLabel label = new JLabel("Witaj!");
 	 private static JLabel label_bye = new JLabel("Żegnaj!");
+	 private static JButton button = new JButton("Click me!");
 	public static void testingUser() 
 	{
 			JFrame myFrame = gui.getFrame();
@@ -22,13 +23,10 @@ public class userTesting extends JFrame{
 	    	
 	    	
 	    	
-	    	
 	    	if(sign.equals("y")||sign.equals("yes")||sign.equals("Y")||sign.equals("YES")) {
 	    		myFrame.add(label);
-		    	label.setVisible(true);
 	    	}else {
 	    		myFrame.add(label_bye);
-	    		label_bye.setVisible(true);
 	    		//System.out.println("I will now close");
 	    		//System.exit(0);
 	    	}
