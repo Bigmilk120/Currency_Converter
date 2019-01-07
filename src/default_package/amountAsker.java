92060a5311297dbd8e390d7110d358339535d9e5
Bigmilk120 <sprawdzany96@gmail.com>
2019-01-07 18:20:28 +0100

    Currency Converter
    Text Field added.

--- src/default_package/amountAsker.java
+++ src/default_package/amountAsker.java
@@ -7,22 +7,28 @@ import javax.swing.*;
 public class amountAsker {
 
 	static JLabel label = new JLabel("How much money do you want to change?");
+	static JTextField amount = new JTextField(20);
 	
 	public static double askForAmount(){
 		System.out.println("Halloooo");
 		
-
+		
 		JFrame myFrame = gui.getFrame();
 		JPanel mainPanel = gui.getPanel();
 		
+		
+		
+		
 		mainPanel.add(label);
+		mainPanel.add(amount);
 		
 		
-		@SuppressWarnings("resource")
+		/*@SuppressWarnings("resource")
 		Scanner reader = new Scanner(System.in);
-		double money=reader.nextDouble();
+		double money=reader.nextDouble(); */
 		
-		mainPanel.repaint();
-		return money;
+
+		myFrame.setVisible(true);
+		return 100;
 	}
 }
